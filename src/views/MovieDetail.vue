e9fcfc909c24570ff814e2f36a760fe22c84acf6
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-09-10 18:42:31 +0200

    Added UI and integrated the API

---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="movie-detail">
+        <h2>{{ movie.Title }}</h2>
+        <p>{{ movie.Year }}</p>
+        <img :src="movie.Poster" alt="Movie poster" class="featured-img"/>
+        <div class="info">
+            <div class="movie-info">
+                <p>Cast: {{ movie.Actors }}</p>
+                <p>Director: {{ movie.Director }}</p>
+                <p>Genre: {{ movie.Genre }}</p>
+            </div>
+            <p class="desc">{{ movie.Plot }}</p>
+        </div>
+    </div>
+</template>
+ 
+<script> 
+import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import env from '@/env';
+
+export default {
+    setup() {
+        const movie = ref({});
+        const route = useRoute();
+
+        onBeforeMount(() => {
+            fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+                .then(response => response.json())
+                .then(data => {
+                    movie.value = data 
+                })
+            }
+        )
+
+        return {
+            movie    
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.movie-detail {
+    padding: 16px;
+
+    h2 {
+        color: #FFF;
+        font-size: 28px;
+        font-weight: 600;
+        margin-bottom: 16px;
+    }
+
+    .featured-img {
+        display: block;
+        max-width: 600px;
+        margin-bottom: 16px;
+        height: 500px;
+    }
+
+    .info {
+        display: flex;
+        gap: 5;
+
+        .movie-info {
+            float: left;
+            width: 50%;
+        }
+
+        .desc {
+            float: right;
+            width: 50%;
+            color: #FFF;
+            font-size: 18px;
+            line-height: 1.4;
+        }
+    }
+}
+</style>
