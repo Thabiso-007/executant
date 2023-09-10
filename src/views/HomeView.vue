9a696dceecb50c89a0466eff7bacc8392adef2b0
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-09-10 18:42:12 +0200

    Modified UI and integrated the API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,209 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="feature-card">
+      <router-link to="/movies/1234">
+        <img 
+          src="https://images.unsplash.com/photo-1642456074142-92f75cb84533?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" 
+          alt="picture" 
+          class="featured-img"
+        />
+        <div class="detail">
+          <h3>Spider man</h3>
+          <p>A strange black entity from another world bonds with Peter Parker and causes inner turmoil as he contends with new villains, temptations, and revenge. A strange black entity from another world bonds with Peter Parker and causes inner turmoil as he contends with new villains, temptations, and reveng</p>
+        </div>
+      </router-link>
+    </div>
+    <form class="search-box">
+      <input type="text" placeholder="What are you looking for?" v-model="search" />
+      <button @click="SearchMovies" type="button">Search</button>
+    </form>
+    <div class="movie-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster">
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
-</template>
+</template> 
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue';
+import env from '@/env';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    const search = ref("");
+    const movies = ref([]);
+
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = "";
+          })          
+      }
+    }
+
+    return {
+      search,
+      movies,
+      SearchMovies
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px; 
+      object-fit: cover;  
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #FFF;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #FFF;
+      }
+    }
+  }
+
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+
+    input {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      &[type="text"] {
+        width: 100%;
+        color: #FFF;
+        background-color: #505050;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
+
+        &::placeholder {
+          color: #f3f3f3;
+        }
+
+        &:focus {
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+        }
+      }
+    }
+    
+    button {
+      width: 100%;
+      max-width: 300px;
+      background-color: #07003c;
+      padding: 8px;
+      border: none;
+      border-radius: 8px;
+      color: #FFF;
+      font-size: 20px;
+      transition: 0.4s;
+
+      &:active {
+        background-color: #3BB070;
+      }
+    }
+  }
+
+  .movie-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 50%;
+      flex: 1 1 30%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42BB83;
+            color: #FFF;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          .year {
+            color: #AAA;
+            font-size: 14px;
+          }
+
+          h3 {
+            color: #FFF;
+            font-weight: 600;
+            font-size: 18px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+  
